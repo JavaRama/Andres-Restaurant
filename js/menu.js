bcaead6054a90cdc927925ac98ee3961d384e6e5
stevenLien46 <steven.lien46@gmail.com>
2014-12-07 13:08:50 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+//Creates an onload function, using JQuery 
+//Used the fade in function to animate the menu to appear when the page is loaded.
 $(document).ready(function(){
 
     $('.slmenu').fadeIn(3000);
